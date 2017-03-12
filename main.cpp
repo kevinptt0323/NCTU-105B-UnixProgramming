6a5677dded336b29732307f5a59d7369adfa511e
Kevin Lin <kevinptt0323@gmail.com>
2017-03-13 04:02:41 +0800

    add getopt and filter

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cctype>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <getopt.h>
 
 #include <iostream>
 #include <sstream>
@@ -17,6 +18,13 @@ const char* TCP_FILE  = "/proc/net/tcp";
 const char* TCP6_FILE = "/proc/net/tcp6";
 const char* UDP_FILE  = "/proc/net/udp";
 const char* UDP6_FILE = "/proc/net/udp6";
+
+const char* short_options = "tu";
+struct option long_options[] = {
+     { "tcp", no_argument, NULL, 't'},
+     { "udp", no_argument, NULL, 'u'},
+	 {  NULL, no_argument, NULL, 0}
+};
 
 map<int,string> inode2pid;
 
@@ -93,6 +101,24 @@ vector<string> getfd() {
 	return fd;
 }
 
+string get_cmd(string pid) {
+	string path = string("/proc/") + pid + "/cmdline";
+	FILE* fin = fopen(path.c_str(), "rb");
+	char* buf = new char[1024];
+	size_t len = fread(buf, sizeof(char), 1023, fin);
+	for(size_t i=0; i<len; i++)
+		if (buf[i]==0)
+			buf[i] = ' ';
+	buf[len] = '\0';
+	string s = buf;
+	delete[] buf;
+	return s;
+}
+
+bool match(const char* str, const char* pat) {
+	return strstr(str, pat)!=NULL;
+}
+
 ostream& operator<<(ostream& out, const Connection& conn) {
 	ostringstream oss;
 	oss << setw(23) << left << conn.local << " "
@@ -104,7 +130,7 @@ ostream& operator<<(ostream& out, const Connection& conn) {
 	return out << oss.str();
 }
 
-void show_tcp() {
+void show_tcp(const string& filter) {
 	cout << "List of TCP connections:" << endl;
 	cout << setw(5)  << left << "Proto" << " "
 		 << setw(23) << left << "Local Address" << " "
@@ -113,15 +139,23 @@ void show_tcp() {
 		 << endl;
 	auto tcp_conn = parse(TCP_FILE);
 	for(const auto& conn: tcp_conn) {
-		cout << setw(5) << left << "tcp" << " " << conn << endl;
+		stringstream ss_conn;
+		ss_conn << conn;
+		string tmp = ss_conn.str();
+		if (filter.size()==0 || match(tmp.c_str(), filter.c_str()))
+			cout << setw(5) << left << "tcp" << " " << tmp << endl;
 	}
 	auto tcp6_conn = parse(TCP6_FILE, 6);
 	for(const auto& conn: tcp6_conn) {
-		cout << setw(5) << left << "tcp6" << " " << conn << endl;
+		stringstream ss_conn;
+		ss_conn << conn;
+		string tmp = ss_conn.str();
+		if (filter.size()==0 || match(tmp.c_str(), filter.c_str()))
+			cout << setw(5) << left << "tcp6" << " " << tmp << endl;
 	}
 }
 
-void show_udp() {
+void show_udp(const string& filter) {
 	cout << "List of UDP connections:" << endl;
 	cout << setw(5)  << left << "Proto" << " "
 		 << setw(23) << left << "Local Address" << " "
@@ -130,27 +164,76 @@ void show_udp() {
 		 << endl;
 	auto udp_conn = parse(UDP_FILE);
 	for(const auto& conn: udp_conn) {
-		cout << setw(5) << left << "udp " << " " << conn << endl;
+		stringstream ss_conn;
+		ss_conn << conn;
+		string tmp = ss_conn.str();
+		if (filter.size()==0 || match(tmp.c_str(), filter.c_str()))
+			cout << setw(5) << left << "udp " << " " << tmp << endl;
 	}
 		auto udp6_conn = parse(UDP6_FILE, 6);
 	for(const auto& conn: udp6_conn) {
-		cout << setw(5) << left << "udp6" << " " << conn << endl;
+		stringstream ss_conn;
+		ss_conn << conn;
+		string tmp = ss_conn.str();
+		if (filter.size()==0 || match(tmp.c_str(), filter.c_str()))
+			cout << setw(5) << left << "udp6" << " " << tmp << endl;
 	}
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	string filter = "";
+	bool f_tcp = true, f_udp = true;
+	{
+		int c;
+		bool _f_tcp = false, _f_udp = false;
+		while( (c=getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+			switch(c) {
+				case 't':
+					_f_tcp = true;
+					break;
+				case 'u':
+					_f_udp = true;
+					break;
+				case '?':
+				default:
+					cout << "~~" << c << endl;
+					break;
+			}
+		}
+		if (_f_tcp || _f_udp)
+			f_tcp = _f_tcp, f_udp = _f_udp;
+		if (optind<argc) {
+			filter = argv[optind];
+		}
+	}
 	{
 		inode2pid.clear();
 		auto fd = getfd();
 		struct stat stat_buf;
 		for(const auto& dir: fd) {
 			stat(dir.c_str(), &stat_buf);
-			if (S_ISSOCK(stat_buf.st_mode))
-				inode2pid[stat_buf.st_ino] = dir;
+			if (S_ISSOCK(stat_buf.st_mode)) {
+				int cnt = 0;
+				int st = 0, ed = 1;
+				for(size_t i=0; i<dir.size(); i++) {
+					if (dir[i]=='/') {
+						cnt++;
+						if (cnt==2) st = i;
+						if (cnt==3) ed = i;
+					}
+				}
+				string pid = dir.substr(st+1, ed-st-1);
+				inode2pid[stat_buf.st_ino] = pid + "/" + get_cmd(pid);
+			}
 		}
 	}
-	show_tcp();
-	cout << endl;
-	show_udp();
+	if (f_tcp) {
+		show_tcp(filter);
+		cout << endl;
+	}
+	if (f_udp) {
+		show_udp(filter);
+		cout << endl;
+	}
 	return 0;
 }
