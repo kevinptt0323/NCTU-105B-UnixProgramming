b7e9ffd76b45405d0cdd84a41aa9192cf94d1f1f
Kevin Lin <kevinptt0323@gmail.com>
2017-03-13 02:55:07 +0800

    add ipv6

--- connection.h
+++ connection.h
@@ -2,15 +2,16 @@
 #define __CONNECTION_H__
 
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 using namespace std;
 
-union IP {
+union IPv4 {
 	unsigned char digit[4];
 	unsigned raw;
 };
 
-ostream& operator<<(ostream& out, const IP& ip) {
+ostream& operator<<(ostream& out, const IPv4& ip) {
 	ostringstream oss;
 	oss << (int)ip.digit[0] << "."
 		<< (int)ip.digit[1] << "."
@@ -19,14 +20,59 @@ ostream& operator<<(ostream& out, const IP& ip) {
 	return out << oss.str();
 }
 
+union IPv6 {
+	unsigned short digit[8];
+	__uint128_t raw;
+};
+
+ostream& operator<<(ostream& out, const IPv6& ip) {
+	ostringstream oss;
+	bool first = true;
+	int cnt = 0;
+	bool double_colon = false;
+	for(int i=7; i>=0; i--) {
+		if (double_colon || ip.digit[i]) {
+			if (first && cnt>=2) {
+				oss << ":";
+				double_colon = true;
+			}
+			if (cnt>=1) {
+				oss << ":";
+				if (!first) double_colon = true;
+			}
+			oss << hex << ip.digit[i];
+			if (i!=0)
+				oss << ":";
+			first = false;
+			cnt = 0;
+		} else cnt++;
+	}
+	if (cnt) {
+		oss << (first ? "::" : ":");
+	}
+	return out << oss.str();
+}
+
 struct Address {
-	IP ip;
+	union {
+		IPv4 v4;
+		IPv6 v6;
+	} ip;
+	int type;
 	unsigned short port;
 };
 
 ostream& operator<<(ostream& out, const Address& addr) {
 	ostringstream oss;
-	oss << addr.ip << ":";
+	switch(addr.type) {
+		case 4:
+			oss << addr.ip.v4;
+			break;
+		case 6:
+			oss << addr.ip.v6;
+			break;
+	}
+	oss << ":";
 	if (addr.port==0) oss << "*";
 	else oss << addr.port;
 	return out << oss.str();
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstring>
+#include <cctype>
 #include <dirent.h>
 #include <sys/stat.h>
 
@@ -12,22 +13,59 @@
 #include "connection.h"
 using namespace std;
 
-const char* TCP_FILE = "/proc/net/tcp";
-const char* UDP_FILE = "/proc/net/udp";
+const char* TCP_FILE  = "/proc/net/tcp";
+const char* TCP6_FILE = "/proc/net/tcp6";
+const char* UDP_FILE  = "/proc/net/udp";
+const char* UDP6_FILE = "/proc/net/udp6";
 
 map<int,string> inode2pid;
 
-vector<Connection> parse(const char* filename) {
+void transform_endian(char* s) {
+	for(int i=0; i<32; i+=8) {
+		swap(s[i], s[i+6]);
+		swap(s[i+1], s[i+7]);
+		swap(s[i+2], s[i+4]);
+		swap(s[i+3], s[i+5]);
+	}
+}
+
+__uint128_t atolll(const char* s) {
+	__uint128_t v = 0;
+	for(int i=0; (s[i]>='0' && s[i]<='9') || (tolower(s[i])>='a'&&tolower(s[i])<='f'); i++)
+		v = (v<<4) + (s[i]>='0'&&s[i]<='9' ? s[i]-48 : tolower(s[i])-'a'+10);
+	return v;
+}
+
+vector<Connection> parse(const char* filename, const int& type = 4) {
 	FILE* fin = fopen(filename, "r");
 	char* buffer = new char[1024];
+	char* local_addr = new char[1024];
+	char* remote_addr = new char[1024];
 	vector<Connection> ret;
 	fgets(buffer, 1023, fin);
 	while (fgets(buffer, 1023, fin)) {
 		Connection conn;
-		sscanf(buffer, "%*d: %x:%hx %x:%hx %*s %*s %*s %*s %*s %*s %d", &conn.local.ip.raw, &conn.local.port, &conn.remote.ip.raw, &conn.remote.port, &conn.inode);
+		sscanf(buffer, "%*d: %s %s %*s %*s %*s %*s %*s %*s %d", local_addr, remote_addr, &conn.inode);
+		if (type==4) {
+			sscanf(local_addr, "%x:%hx", &conn.local.ip.v4.raw, &conn.local.port);
+			sscanf(remote_addr, "%x:%hx", &conn.remote.ip.v4.raw, &conn.remote.port);
+		}
+		else if (type==6) {
+			sscanf(local_addr, "%*x:%hx", &conn.local.port);
+			sscanf(remote_addr, "%*x:%hx", &conn.remote.port);
+			transform_endian(local_addr);
+			transform_endian(remote_addr);
+			conn.local.ip.v6.raw = atolll(local_addr);
+			conn.remote.ip.v6.raw = atolll(remote_addr);
+		}
+		conn.local.type = type;
+		conn.remote.type = type;
 		ret.emplace_back(conn);
 	}
 	fclose(fin);
+	delete[] buffer;
+	delete[] local_addr;
+	delete[] remote_addr;
 	return ret;
 }
 
@@ -57,11 +95,12 @@ vector<string> getfd() {
 
 ostream& operator<<(ostream& out, const Connection& conn) {
 	ostringstream oss;
-	oss << setw(23) << left << conn.local << " " << setw(23) << left << conn.remote;
+	oss << setw(23) << left << conn.local << " "
+		<< setw(23) << left << conn.remote << " ";
 	if (inode2pid.find(conn.inode)!=inode2pid.end())
-		oss << " " << setw(30) << left << inode2pid[conn.inode];
+		oss << setw(30) << left << inode2pid[conn.inode];
 	else
-		oss << " " << setw(30) << left << "-";
+		oss << setw(30) << left << "-";
 	return out << oss.str();
 }
 
@@ -76,13 +115,26 @@ void show_tcp() {
 	for(const auto& conn: tcp_conn) {
 		cout << setw(5) << left << "tcp" << " " << conn << endl;
 	}
+	auto tcp6_conn = parse(TCP6_FILE, 6);
+	for(const auto& conn: tcp6_conn) {
+		cout << setw(5) << left << "tcp6" << " " << conn << endl;
+	}
 }
 
 void show_udp() {
 	cout << "List of UDP connections:" << endl;
+	cout << setw(5)  << left << "Proto" << " "
+		 << setw(23) << left << "Local Address" << " "
+		 << setw(23) << left << "Remote Address" << " "
+		 << setw(30) << left << "PID/Program name and arguments"
+		 << endl;
 	auto udp_conn = parse(UDP_FILE);
 	for(const auto& conn: udp_conn) {
 		cout << setw(5) << left << "udp " << " " << conn << endl;
+	}
+		auto udp6_conn = parse(UDP6_FILE, 6);
+	for(const auto& conn: udp6_conn) {
+		cout << setw(5) << left << "udp6" << " " << conn << endl;
 	}
 }
 
