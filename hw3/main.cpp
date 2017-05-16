040720a15bebc95a179e80698375d49b1b2cd691
Kevin Lin <kevinptt0323@gmail.com>
2017-05-16 21:22:54 +0800

    finish command execute

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,9 +1,17 @@
 #include <cstdio>
 #include <cstring>
 #include <vector>
+#include <array>
 #include <string>
 
+#include <unistd.h>
+
 #define BUF_SIZE 1024
+#define error(STR, ...) \
+	fprintf(stderr, "%s: " STR "\n", SHELL_NAME, ##__VA_ARGS__); \
+	exit(EXIT_FAILURE)
+
+const char* SHELL_NAME = "hw3sh";
 
 using namespace std;
 
@@ -32,19 +40,65 @@ vector<vector<string>> parse(char* cmd) {
 	return cmds;
 }
 
-int execute(const vector<vector<string>>& cmds) {
-	for(const auto& cmd: cmds) {
-		for(const auto& argv: cmd) {
-			printf("%s ", argv.c_str());
+pid_t create_process(const vector<string>& argv0, int fd_in, int fd_out, vector<array<int,2>>& pipes_fd) {
+	pid_t pid = fork();
+	if (pid<0) {
+		error("can't fork");
+	} else if (pid==0) {
+		if (fd_in != STDIN_FILENO)
+			dup2(fd_in, STDIN_FILENO);
+		if (fd_out != STDOUT_FILENO)
+			dup2(fd_out, STDOUT_FILENO);
+
+		for(auto& pipe_fd: pipes_fd) {
+			close(pipe_fd[0]);
+			close(pipe_fd[1]);
 		}
-		puts("");
+
+		char** argv = new char*[argv0.size()+1];
+		for(size_t i=0; i<argv0.size(); i++) {
+			argv[i] = new char[argv0[i].size()+1];
+			strcpy(argv[i], argv0[i].c_str());
+		}
+		argv[argv0.size()] = NULL;
+
+		if (execvp(argv[0], argv) == -1) {
+			error("command not found: %s", argv[0]);
+		}
+	} else {
+		return pid;
 	}
+}
+
+int execute(const vector<vector<string>>& cmds) {
+	vector<array<int,2>> pipes_fd(cmds.size()-1);
+	for(auto& pipe_fd: pipes_fd) {
+		if (pipe(pipe_fd.data()) == -1) {
+			error("can't create pipe");
+		}
+	}
+
+	for(size_t i=0; i<cmds.size(); i++) {
+		int fd_in  = i==0 ? STDIN_FILENO : pipes_fd[i-1][0];
+		int fd_out = i==cmds.size()-1 ? STDOUT_FILENO : pipes_fd[i][1];
+		create_process(cmds[i], fd_in, fd_out, pipes_fd);
+	}
+
+	for(auto& pipe_fd: pipes_fd) {
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+	}
+
+	for(size_t i=0; i<cmds.size(); i++) {
+		wait(NULL);
+	}
+
 	//printf("execute: %s\n", cmd);
 	return 0;
 }
 
 int main() {
-	char prompt[] = "hw3sh > ";
+	char prompt[] = "hw3sh> ";
 	char buf[BUF_SIZE+1];
 	while (1) {
 		output_prompt(prompt);
