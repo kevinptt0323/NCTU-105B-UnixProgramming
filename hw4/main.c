aeb88dd6c276830653497b14dfa518fccbf0012a
Kevin Lin <kevinptt0323@gmail.com>
2017-06-18 15:54:42 +0800

    finish connection

--- hw4/main.c
+++ hw4/main.c
@@ -1,9 +1,79 @@
 #include "othello.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 static int width;
 static int height;
 static int cx = 3;
 static int cy = 3;
+
+int start_connection(char* flag, char* arg) {
+	if (strcmp(flag, "-s") == 0) {
+		int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		struct sockaddr_in server, client;
+
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = INADDR_ANY;
+		server.sin_port = htons(atoi(arg));
+
+		if (bind(server_fd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1) {
+			fprintf(stderr, "Could not binding socket\n");
+			return -1;
+		}
+
+		if (listen(server_fd, 128) == -1) {
+			fprintf(stderr, "Could not listen on socket\n");
+			return -1;
+		}
+
+		fprintf(stderr, "listen on %d\n", atoi(arg));
+
+		char buf[2048];
+		int buf_sz;
+		while(1) {
+			socklen_t client_len = sizeof(client);
+			int client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
+
+			if (client_fd < 0) {
+				fprintf(stderr, "Could not establish new connection\n");
+				return -1;
+			}
+
+			return client_fd;
+
+			while(1) {
+				if ((buf_sz=recv(client_fd, buf, 2047, 0)) > 0) {
+					buf[buf_sz] = 0;
+					printf("recv: %s\n", buf);
+				}
+			}
+		}
+	} else if (strcmp(flag, "-c") == 0) {
+		int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		struct sockaddr_in server;
+		char ip[20], port[10];
+		for(int i=0, i2=-1; arg[i]; ++i) {
+			if (arg[i]!=':') {
+				if (i2==-1) ip[i] = arg[i], ip[i+1] = 0;
+				else port[i2++] = arg[i], port[i2] = 0;
+			} else {
+				i2 = 0;
+			}
+		}
+		if (strcmp(ip, "localhost") == 0) strcpy(ip, "127.0.0.1");
+
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = inet_addr(ip);
+		server.sin_port = htons(atoi(port));
+
+		if (connect(server_fd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1) {
+			fprintf(stderr, "Could not connect to %s\n", arg);
+			return -1;
+		}
+
+		return server_fd;
+	} else return -1;
+}
 
 const int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
 const int dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
@@ -27,6 +97,12 @@ void update_board(int _cx, int _cy, int player) {
 int
 main(int argc, char* argv[])
 {	
+	if (argc!=3) return 0;
+	int fd = start_connection(argv[1], argv[2]);
+	if (fd<0) return 0;
+	printf("%d\n", fd);
+	close(fd);
+	return 0;
 	initscr();			// start curses mode 
 	getmaxyx(stdscr, height, width);// get screen size
 
